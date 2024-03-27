12971c0e78962e3d8a7f4fe36e7cd8d7991b61e5
ITLSupress <itlesports@outlook.com>
2024-03-26 21:44:35 -0700

    RPG Addon GitHub Test
    Update .gitignore

--- src/main/java/btw/community/arminias/rpg/RPGAddon.java
+++ src/main/java/btw/community/arminias/rpg/RPGAddon.java
@@ -12,6 +12,7 @@ import net.minecraft.src.Packet250CustomPayload;
 import java.io.*;
 
 public class RPGAddon extends BTWAddon {
+    //Josh If You See This It's Working
     private static RPGAddon instance;
 
     public static RPGPointsAllocation pointsAllocation = null;
